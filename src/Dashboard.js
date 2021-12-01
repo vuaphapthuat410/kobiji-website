9b3d50e78991ca8a638b5c86bb11452faf7e8a4d
hoanganh121199 <anhvmh.vnist@gmail.com>
2021-12-01 20:56:11 +0700

    thêm calendar

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,55 @@
 import * as React from "react";
 import { Card, CardContent, CardHeader } from '@material-ui/core';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment'
+import 'moment/locale/ja'
 
-export default () => (
-    <Card>
+const localizer = momentLocalizer(moment)
+const events = [
+    {
+      id: 0,
+      title: 'Meeting',
+      start: moment({ hours: 8 }).toDate(),
+      end: moment({ hours: 10 }).toDate(),
+    },
+    {
+      id: 1,
+      title: 'Lunch',
+      start: moment({ hours: 12 }).toDate(),
+      end: moment({ hours: 13 }).toDate()
+    },
+    {
+      id: 2,
+      title: 'Coding',
+      start: moment({ hours: 14 }).toDate(),
+      end: moment({ hours: 17 }).toDate(),
+    },
+  ];
+function Dashboard(){
+    return(
+        <Card>
         <CardHeader title="Welcome to the administration" />
         <CardContent>kobiji</CardContent>
+        <Calendar
+        culture = 'ja'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        messages={{
+            'today':"今日",
+            'previous':"前",
+            'next':"次",
+            'month':"月",
+            'week':"週",
+            'day':"日",
+            "showMore":total => `${total}もっと見せる`,
+            'agenda':"議題"
+        }}
+      />
     </Card>
-);
+    )
+}
+export default Dashboard
