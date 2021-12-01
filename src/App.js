7d5279c823845005c5ffe509a74bbcdd1fa749cd
hoanganh121199 <anhvmh.vnist@gmail.com>
2021-12-02 00:07:10 +0700

    phân quyền admin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import React, { useEffect, useState } from "react";
+import { Admin, Resource, ListGuesser, EditGuesser , useGetIdentity} from "react-admin";
 import Dashboard from "./Dashboard";
 import { PostList, PostShow, PostCreate, PostEdit } from "./posts";
 import { UserList, UserShow, UserCreate, UserEdit } from "./users";
@@ -12,47 +12,91 @@ import CustomLoginPage from "./CustomLoginPage";
 import { authProvider, dataProvider, auth } from "./firebase";
 
 function App() {
-  return (
-    <Admin
-      dataProvider={dataProvider}
-      dashboard={Dashboard}
-      authProvider={authProvider}
-      loginPage={CustomLoginPage}
-    >
-      {/* <Resource
-        name="posts"
-        icon={PostIcon}
-        list={PostList}
-        show={PostShow}
-        create={PostCreate}
-        edit={PostEdit}
-      /> */}
-      <Resource
-        name="users"
-        icon={UserIcon}
-        list={UserList}
-        show={UserShow}
-        create={UserCreate}
-        edit={UserEdit}
-      />
-      <Resource
-        name="talents"
-        icon={UserIcon}
-        list={TalentList}
-        show={TalentShow}
-        create={TalentCreate}
-        edit={TalentEdit}
-      />
-      <Resource
-        name="accounts"
-        icon={UserIcon}
-        list={AccountList}
-        show={AccountShow}
-        create={AccountCreate}
-        edit={AccountEdit}
-      />
-    </Admin>
-  );
+  const [email,setEmail] = useState("")
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user){
+      setEmail(user.email)
+      }
+    });
+  }, []);
+  if (email ==='admin@gmail.com'){
+    return (
+      <Admin
+        dataProvider={dataProvider}
+        dashboard={Dashboard}
+        authProvider={authProvider}
+        loginPage={CustomLoginPage}
+      >
+        {/* <Resource
+          name="posts"
+          icon={PostIcon}
+          list={PostList}
+          show={PostShow}
+          create={PostCreate}
+          edit={PostEdit}
+        /> */}
+        <Resource
+          name="users"
+          icon={UserIcon}
+          list={UserList}
+          show={UserShow}
+          create={UserCreate}
+          edit={UserEdit}
+        />
+        <Resource
+          name="talents"
+          icon={UserIcon}
+          list={TalentList}
+          show={TalentShow}
+          create={TalentCreate}
+          edit={TalentEdit}
+        />
+        <Resource
+          name="accounts"
+          icon={UserIcon}
+          list={AccountList}
+          show={AccountShow}
+          create={AccountCreate}
+          edit={AccountEdit}
+        />
+      </Admin>
+    );
+  } else {
+    return (
+      <Admin
+        dataProvider={dataProvider}
+        dashboard={Dashboard}
+        authProvider={authProvider}
+        loginPage={CustomLoginPage}
+      >
+        {/* <Resource
+          name="posts"
+          icon={PostIcon}
+          list={PostList}
+          show={PostShow}
+          create={PostCreate}
+          edit={PostEdit}
+        /> */}
+        <Resource
+          name="users"
+          icon={UserIcon}
+          list={UserList}
+          show={UserShow}
+          create={UserCreate}
+          edit={UserEdit}
+        />
+        <Resource
+          name="talents"
+          icon={UserIcon}
+          list={TalentList}
+          show={TalentShow}
+          create={TalentCreate}
+          edit={TalentEdit}
+        />
+      </Admin>
+    );
+  }
 }
 
 export default App;
