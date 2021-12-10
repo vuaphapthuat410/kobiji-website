8daafde3d4a1eab72bb8892c22af669e11f15760
vuaphapthuat410 <vuaphapthuat410@gmail.com>
2021-12-10 11:07:22 +0700

    fix info header

--- src/infoHeader.js
+++ src/infoHeader.js
@@ -2,35 +2,37 @@ import * as React from "react";
 import { auth } from "./firebase";
 import firebase from "firebase";
 
-var users = { name: "admin", role: "アドミン" } ; 
+var users = new Map();
+users.set("admin@gmail.com", { name: "admin", role: "アドミン" });
 var db = firebase.firestore();
 db.collection("accounts")
   .get()
   .then((snapshot) => {
     snapshot.forEach((doc) => {
-      users = {
+      users.set(doc.data().mail, {
         name: doc.data().name,
         role: doc.data().role,
-      }
+      });
     });
   });
 db.collection("talents")
   .get()
   .then((snapshot) => {
     snapshot.forEach((doc) => {
-      users = {
+      users.set(doc.data().mailAddress, {
         name: doc.data().name,
         role: "タレント",
-      }
+      });
     });
   });
 
 const Info = () => {
-    return (
-        <>
-            {users.name} - {users.role}
-        </>
-    )
+  let account_id = auth.currentUser?.email ?? "";
+  return (
+      <>
+          {users.get(account_id)?.name} - {users.get(account_id)?.role}
+      </>
+  )
 }
 
 export default Info
