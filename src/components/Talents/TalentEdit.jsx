50478649e3417ec645de69dadd96231224d3c63a
vuaphapthuat410 <vuaphapthuat410@gmail.com>
2022-01-10 14:52:00 +0700

    fix talent buy

--- src/components/Talents/TalentEdit.jsx
+++ src/components/Talents/TalentEdit.jsx
@@ -1,10 +1,14 @@
 import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   DateInput,
   Edit,
   ListButton, SaveButton, SelectInput, SimpleForm,
-  TextInput, Toolbar, TopToolbar
+  TextInput, Toolbar, TopToolbar,
+  Loading,
+  useDataProvider,
+  useMutation,
+  useRedirect,
 } from "react-admin";
 import { countryList, genderList, statusList } from "../../utils/list";
 import useContext from "../../db/useContext";
@@ -12,15 +16,28 @@ import {
   validateBirthday,
   validateCountry, validateGender,
   validateName,
+  validateRequired,
   validateStatus
 } from "../../utils/validate";
+import { auth } from "../../db/firebase";
 
 function userChoices(users) {
   let client = []
-  for (let user in users) 
-    if (user.role === "クライアント") 
-      client.append({ id: user.mail, name: `${user.name} - ${user.mail}`});
-  
+  // for (let user in users) {
+  //   if (user.role === "クライアント") {
+  //     client.append({ id: user.mail, name: `${user.name} - ${user.mail}`});
+  //     console.log(client)
+  //   }
+  //   console.log(user.mail)
+  // } 
+  for (const [id, user] of users.entries()) {
+    if (user.role === "クライアント") {
+      client.push({ id: user.mail, name: `${user.name} - ${user.mail}`});
+      console.log(client)
+    }
+    console.log(user.mail)
+  }
+
   return client
 }
 
@@ -48,6 +65,9 @@ const CreateToolbar = (props) => {
         label="変更"
         redirect="show"
         transform={(data) => {
+          if (data.client !== undefined && data.client !== "") {
+            data.status = "売れた"
+          }
           return data;
         }}
         submitOnEnter={true}
@@ -57,12 +77,62 @@ const CreateToolbar = (props) => {
 };
 
 const TalentEdit = (props) => {
-  const [{ users }, loading] = useContext();
+  const [{ users }] = useContext();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [clients, setClients] = useState([]);
 
-  if (loading) return <></>;
+  const currentUser = auth.currentUser;
+  const redirect = useRedirect();
+  const [mutate] = useMutation();
+  const dataProvider = useDataProvider();
+
+  React.useEffect(() => {
+    dataProvider
+      .getList("accounts", {
+        pagination: { page: 1, perPage: 100 },
+        filter: {},
+      })
+      .then(({ data }) => {
+        setUser(data.find((_user) => _user.mail === currentUser.email));
+        setClients(data.filter((_client) => _client.role === "クライアント"))
+        setLoading(false);
+      });
+  }, [dataProvider, currentUser]);
+
+  const onSuccess = async ({ data }) => {
+    // Add talents to client field
+    var client = clients.find((_user) => _user.mail === data.client);
+    let own = client?.own ?? [];
+    own.push(data.mail);
+    await mutate({
+      type: "update",
+      resource: "accounts",
+      payload: {
+        id: client.id,
+        data: {
+          own,
+        },
+      },
+    });
+    await redirect(`/accounts/`);
+  };
+
+  if (loading) return <Loading />;
 
   return (
-    <Edit {...props} actions={<EditActionList />}>
+    <Edit 
+      {...props} 
+      actions={<EditActionList />}
+      onSuccess={onSuccess}
+      transform={(data) => {
+        if (data.client !== undefined && data.client !== "") {
+          data.status = "売れた"
+        }
+        return data;
+      }}
+      mutationMode="pessimistic"
+    >
       <SimpleForm toolbar={<CreateToolbar />}>
         {/* <TextInput source="id" label="ID" /> */}
         <TextInput source="name" label="名前" validate={validateName} />
@@ -93,6 +163,11 @@ const TalentEdit = (props) => {
           label="国籍"
           choices={countryList}
           validate={validateCountry}
+        />
+        <SelectInput
+          source="client"
+          label="クライアントメール"
+          choices={userChoices(users)}
         />
       </SimpleForm>
     </Edit>
